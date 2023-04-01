7b725540cc92d60192e7a3fccb5fd0b2b58aed81
Ryan Palmer <rpalmer9696@gmail.com>
2023-04-01 07:04:36 +0100

    Adds user config

---
+++ lua/user/init.lua
@@ -0,0 +1,8 @@
+return {
+  colorscheme = "tokyonight",
+  plugins = {
+    "AstroNvim/astrocommunity",
+    { import = "astrocommunity.pack.rust" },
+    { import = "astrocommunity.colorscheme.tokyonight" }
+  }
+}
