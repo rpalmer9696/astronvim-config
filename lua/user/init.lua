c7ffea8542d03ff61ae23c010e4ad34b1ff88c7b
Ryan Palmer <rpalmer9696@gmail.com>
2023-04-16 14:30:12 +0100

    Updates theme

--- lua/user/init.lua
+++ lua/user/init.lua
@@ -1,8 +1,10 @@
 return {
-  colorscheme = "tokyonight",
+  colorscheme = "nightfox",
   plugins = {
     "AstroNvim/astrocommunity",
     { import = "astrocommunity.pack.rust" },
-    { import = "astrocommunity.colorscheme.tokyonight" }
+    { import = "astrocommunity.colorscheme.tokyonight" },
+    { import = "astrocommunity.colorscheme.nightfox" },
+    { import = "astrocommunity.colorscheme.catppuccin" }
   }
 }
